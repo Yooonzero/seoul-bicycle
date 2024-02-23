4b8f92fd7049f588ef2ca55720101eeb57cf1674
Yoonzero <playlist808@kakao.com>
2024-02-23 23:55:49 +0900

    add console

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express();
 const HOST = '127.0.0.1';
 const PORT = 3000;
 
-app.listen(PORT, HOST);
+// app.listen(PORT, HOST);
 
-// app.listen(PORT, HOST, () => {
-//     console.log(`${PORT} 서버가 열렸습니다.`);
-// });
+app.listen(PORT, HOST, () => {
+    console.log(`Server is Listening...`, PORT);
+});
